0bf9c64cc82458567547d7f74a6c0fd3248ac509
Felipe Campos <fmrcampos@berkeley.edu>
2022-02-28 07:49:12 -0800

    Initial Commit

---
+++ AutoFlowUI/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AutoFlowUI
+//
+//  Created by Felipe Campos on 2/28/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
